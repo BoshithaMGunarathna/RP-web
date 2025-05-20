50fd70fb4434f4ab1845aeb484540dc52d527997
Boshitha Gunarathna <IT21319792@my.sliit.lk>
2025-05-20 14:16:53 +0530

    Update About.tsx

---
+++ project-website/src/pages/About.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const About: React.FC = () => {
+ return (
+    <div>
+      <h1>About Us</h1>
+      <p>We are a team of four students working on an automated interview system...</p>
+      <ul>
+        <li>Deneth – Frontend</li>
+        <li>Indudini – Content</li>
+        <li>Shehela – Documents</li>
+        <li>You (Leader) – Backend, Deployment, About, Contact</li>
+      </ul>
+    </div>
+  )
+};
+
+export default About;
