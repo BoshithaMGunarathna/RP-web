19b714a1e04a8e428139fcd25d33026e0bc4d928
Deneth Pinsara <it21158186@my.sliit.lk>
2025-05-24 17:57:20 +0530

    fix(app): configure routes for not found route

--- project-website/src/App.tsx
+++ project-website/src/App.tsx
@@ -1,9 +1,9 @@
-// App.tsx
 import { Routes, Route } from 'react-router-dom'
 import Layout from './components/layout/Layout'
 import About from './pages/About'
 import Contact from './pages/Contact'
 import Home from './pages/Home'
+import NotFound from './pages/NotFound'
 
 function App() {
   return (
@@ -13,6 +13,8 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         {/* <Route path="*" element={<div>Welcome to Our Research Project</div>} /> */}
+
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Layout>
   )
