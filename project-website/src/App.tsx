6982c4bd56cdcff04303f127ec2af896dbfdcb99
Deneth Pinsara <it21158186@my.sliit.lk>
2025-05-25 14:22:52 +0530

    fix(app): wrap routes with theme provider

--- project-website/src/App.tsx
+++ project-website/src/App.tsx
@@ -1,22 +1,28 @@
-import { Routes, Route } from 'react-router-dom'
-import Layout from './components/layout/Layout'
-import About from './pages/About'
-import Contact from './pages/Contact'
-import Home from './pages/Home'
-import NotFound from './pages/NotFound'
+import { Routes, Route } from 'react-router-dom';
+import Layout from './components/layout/Layout';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+import Blogs from './pages/Blogs';
+import Docs from './pages/Docs';
+import { ThemeProvider } from '../ThemeContext';
 
 function App() {
   return (
-    <Layout>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Layout>
-  )
+    <ThemeProvider>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/blogs" element={<Blogs />} />
+          <Route path="/docs" element={<Docs />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Layout>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
