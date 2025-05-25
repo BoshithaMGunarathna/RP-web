08c755b308a849553246782dd5ae9b86bf6fb62b
Deneth Pinsara <it21158186@my.sliit.lk>
2025-05-25 14:23:28 +0530

    fix(layout): update styles with theme

--- project-website/src/components/layout/Layout.tsx
+++ project-website/src/components/layout/Layout.tsx
@@ -1,28 +1,19 @@
-'use client'
-
-import React, { useState } from 'react'
-import Sidebar from './Sidebar'
+import React from 'react'
 import Topbar from './Topbar'
 import Footer from './Footer'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  const [sidebarOpen, setSidebarOpen] = useState(false)
-
   return (
-    <>
-      <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
-
-      <div className="flex flex-col min-h-screen lg:pl-72">
-        <Topbar setSidebarOpen={setSidebarOpen} />
-        
-        {/* This main flex-grow area will expand to fill space */}
-        <main className="flex-grow py-10">
-          <div className="px-4 sm:px-6 lg:px-8">{children}</div>
-        </main>
-        
-        {/* Footer stays at bottom */}
-        <Footer />
-      </div>
-    </>
+    <div className="min-h-screen flex flex-col bg-white dark:bg-gray-900 text-gray-900 dark:text-gray-100 transition-colors duration-300">
+      <Topbar />
+      
+      {/* This main flex-grow area will expand to fill space */}
+      <main className="flex-grow bg-white dark:bg-gray-900">
+        {children}
+      </main>
+      
+      {/* Footer stays at bottom */}
+      <Footer />
+    </div>
   )
 }
